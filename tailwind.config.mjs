cf4528e588fc33d699fc6a8a9cca579f5e53df4b
Jaime-prog <a00571842@tec.mx>
2024-04-19 01:05:53 -0600

    Añadir iconos de Facebook e Instagram al componente Footer.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,41 +2,48 @@
 module.exports = {
 	darkMode: ["class"],
 	content: [
-	  './pages/**/*.{ts,tsx}',
-	  './components/**/*.{ts,tsx}',
-	  './app/**/*.{ts,tsx}',
-	  './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 	],
 	prefix: "",
 	theme: {
-	  container: {
-		center: true,
-		padding: "2rem",
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			colors: {
+				'cor_white': '#F0F4EF',
+				'cor_strong_green': '#85B6AC',
+				'cor_aqua_white': '#C8E5E0',
+				'cor_brownish': '#B3CDC6',
+				'cor_teal': '#BEDCD2',
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
 		screens: {
-		  "2xl": "1400px",
+			xs: "480px",
+			sm: "768px",
+			md: "1060px",
 		},
-	  },
-	  extend: {
-		keyframes: {
-		  "accordion-down": {
-			from: { height: "0" },
-			to: { height: "var(--radix-accordion-content-height)" },
-		  },
-		  "accordion-up": {
-			from: { height: "var(--radix-accordion-content-height)" },
-			to: { height: "0" },
-		  },
-		},
-		animation: {
-		  "accordion-down": "accordion-down 0.2s ease-out",
-		  "accordion-up": "accordion-up 0.2s ease-out",
-		},
-	  },
-	  screens: {
-		xs: "480px",
-		sm: "768px",
-		md: "1060px",
-	  },
 	},
 	plugins: [require("tailwindcss-animate")],
-  }
+}
